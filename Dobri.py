87eb47507340004095109fef94647c81ba1beea8
Dobromir Dobrev <dobri991@abv.bg>
2021-01-16 20:16:37 +0200

    Adding Dobri file

---
+++ Dobri.py
@@ -0,0 +1,11 @@
+from tkinter import * 
+# import tkinter.messagebox
+# from PIL import ImageTk, Image
+
+root = Tk()
+
+myLabel = Label(root, text="Hello World")
+
+myLabel.pack()
+
+root.mainloop()
