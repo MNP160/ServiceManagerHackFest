fc99750393480c120bba9c7e9358fc56f0b0784e
Chris Tsenov <ktsenov15@gmail.com>
2021-01-17 10:43:07 +0200

    add validation to login_view + upload images

--- login_view.py
+++ login_view.py
@@ -1,8 +1,7 @@
 from tkinter import *
 from tkinter import Text
 
-
-from PIL import Image,ImageTk
+from PIL import Image,ImageTk, ImageOps
 
 root = Tk()
 root.geometry('600x500')
@@ -12,17 +11,17 @@ imge=Image.open("purple-red.jpg")
 photo=ImageTk.PhotoImage(imge.resize((600, 500)))
 
 lab=Label(image=photo)
-lab.pack()
+lab.place(x=0,y=0)
 
+icon = Image.open("icon.jpg")
+icon = ImageOps.fit(icon, (90, 90))
+icon = ImageTk.PhotoImage(icon)
+mylabel = Label(root, image=icon)
 
+mylabel.place(x=265,y=100,height=90,width=90)
 
 email=StringVar()
 password=StringVar()
-    
-
-login = Button(root, text="Login",relief="raised",width=5,font=("arial", 13), bg = "purple", fg='white')
-login.place(x=430,y=330)
-
 
 label_1 = Label(root, text="Email ",width=10,font=("bold", 15), bg = 'purple', fg = 'white', relief = "groove")
 label_1.place(x=120,y=240)
@@ -36,5 +35,22 @@ label_2.place(x=120,y=280)
 
 entry_2 = Entry(root,textvar=password, font=("arial", 14))
 entry_2.place(x=240,y=281)
+
+vld_label = Label(root, text = "Invalid email address or password",width=27,font=("ariel", 10), bg = '#ede7a0', fg = 'black', relief = "solid", borderwidth = 1)
+
+def validate():
+    
+    email_v = email.get()
+    pass_v = password.get()
+    
+    if email_v == 'test' and pass_v == 'test':
+        root.destroy()
+    else:
+        vld_label.place(x=242,y=315)
+
+login = Button(root, text="Login",relief="raised",width=5,font=("arial", 13), bg = "purple", fg='white',command = validate)
+login.place(x=430,y=340)
+
+print
 
 root.mainloop()
