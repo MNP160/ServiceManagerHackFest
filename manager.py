e86a7d6dd5637034294034e40cd871a250b8b72d
mnp160 <mpenev13@gmail.com>
2021-01-17 15:02:45 +0200

    finishing integration

--- manager.py
+++ manager.py
@@ -4,17 +4,20 @@ import requests
 from datetime import datetime
 
 
-services = get_all_services() 
-for service in services:
-    result = requests.get(service.get('url'))
-    print(result.status_code)
-    
-    print(result.elapsed.total_seconds())
-    currentTime=datetime.now()
-    if result.elapsed.total_seconds() > 5:
+def query_services():
 
-        send_mail(service.get('name'), service.get('url'),'timeout')
-        create_log(service.get('name'), service.get('url'),'timeout', currentTime.strftime("%d/%m/%Y %H:%M:%S"))
-    elif result.status_code!='200': 
-        send_mail(service.get('name'), service.get('url'),str(result.status_code))  
-        create_log(service.get('name'), service.get('url'),str(result.status_code), currentTime.strftime("%d/%m/%Y %H:%M:%S"))
+    services = get_all_services() 
+    for service in services:
+        result = requests.get(service.get('url'))
+        #print(result.status_code)
+    
+        print(result.elapsed.total_seconds())
+        currentTime=datetime.now()
+        print(result.text)
+        if result.elapsed.total_seconds() > 5:
+
+            send_mail(service.get('name'), service.get('url'),'timeout')
+            create_log(service.get('name'), service.get('url'),'timeout', currentTime.strftime("%d/%m/%Y %H:%M:%S"))
+        elif result.status_code!='200': 
+            send_mail(service.get('name'), service.get('url'),str(result.status_code))  
+            create_log(service.get('name'), service.get('url'),str(result.status_code), currentTime.strftime("%d/%m/%Y %H:%M:%S"))
